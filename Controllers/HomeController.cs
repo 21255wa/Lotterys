b30ebf5f3f73ed64e014078bf0b83e80cb929b13
韩帅 <roseone829@163.com>
2024-03-08 10:38:04 +0800

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,13 @@ namespace Lottery.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+		public IActionResult LotterysIndex()
+		{
+			return View();
+		}
+		public IActionResult UesrIndex() 
+		{
+			return View();
+		}
 	}
 }
