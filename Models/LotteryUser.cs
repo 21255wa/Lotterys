b52fcfef2750d7c7c5a70085d536fa26c00e0447
韩帅 <roseone829@163.com>
2024-03-08 10:13:00 +0800

    update 实体更改

---
+++ Models/LotteryUser.cs
@@ -0,0 +1,32 @@
+﻿namespace Lottery.Models
+{
+	public class LotteryUser
+	{
+		/// <summary>
+		/// 用户ID
+		/// </summary>
+        public int UserId { get; set; }
+		/// <summary>
+		/// 用户昵称
+		/// </summary>
+
+		public string UserName { get; set; }
+		/// <summary>
+		/// 微信号
+		/// </summary>
+		public string WxNum { get; set; }
+		/// <summary>
+		/// 公司名称
+		/// </summary>
+
+        public string CompanyName { get; set; }
+		/// <summary>
+		///抽奖号码
+		/// </summary>
+		public int LotteryNum { get; set;}
+		/// <summary>
+		/// 抽奖Id
+		/// </summary>
+        public int LotteryId { get; set; }
+    }
+}
